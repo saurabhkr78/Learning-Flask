e43b727c79962c3161f858e94245da4221dd07c9
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-05-20 05:31:42 +0530

    added countdown timer for a todo task to give user a sense of urgency for the following task

--- app.py
+++ app.py
@@ -1,43 +1,61 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+db = SQLAlchemy(app)
 
-todos = []
-next_id = 1
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    timer = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'task': self.task,
+            'timer': self.timer
+        }
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/add', methods=['POST'])
-def add_todo():
-    global next_id
-    task = request.json.get('task')
-    if task:
-        todo = {'id': next_id, 'task': task}
-        todos.append(todo)
-        next_id += 1
-        return jsonify({'success': True, 'id': todo['id']})
-    return jsonify({'success': False})
-
 @app.route('/todos', methods=['GET'])
 def get_todos():
-    return jsonify({'todos': todos})
+    todos = Todo.query.all()
+    return jsonify({'todos': [todo.to_dict() for todo in todos]})
 
-@app.route('/edit/<int:todo_id>', methods=['PUT'])
-def edit_todo(todo_id):
-    task = request.json.get('task')
-    for todo in todos:
-        if todo['id'] == todo_id:
-            todo['task'] = task
-            return jsonify({'success': True})
-    return jsonify({'success': False})
+@app.route('/add', methods=['POST'])
+def add_todo():
+    data = request.json
+    new_todo = Todo(task=data['task'], timer=data['timer'])
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({'success': True, 'id': new_todo.id})
 
-@app.route('/delete/<int:todo_id>', methods=['DELETE'])
-def delete_todo(todo_id):
-    global todos
-    todos = [todo for todo in todos if todo['id'] != todo_id]
-    return jsonify({'success': True})
+@app.route('/edit/<int:id>', methods=['PUT'])
+def edit_todo(id):
+    data = request.json
+    todo = Todo.query.get(id)
+    if todo:
+        todo.task = data['task']
+        db.session.commit()
+        return jsonify({'success': True})
+    return jsonify({'success': False}), 404
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        return jsonify({'success': True})
+    return jsonify({'success': False}), 404
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
